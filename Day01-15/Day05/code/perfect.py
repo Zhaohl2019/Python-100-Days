141a5e4535d99d328c2d4deba5a4db044889d3cf
ZHL <zhaohl1998@163.com>
2019-06-04 01:15:02 +0800

    test day07

--- Day01-15/Day05/code/perfect.py
+++ Day01-15/Day05/code/perfect.py
@@ -10,7 +10,7 @@ Date: 2018-03-02
 import time
 import math
 
-start = time.clock()
+start = time.time()
 for num in range(1, 10000):
     sum = 0
     for factor in range(1, int(math.sqrt(num)) + 1):
@@ -20,7 +20,7 @@ for num in range(1, 10000):
                 sum += num / factor
     if sum == num:
         print(num)
-end = time.clock()
+end = time.time()
 print("执行时间:", (end - start), "秒")
 
 # 通过比较上面两种不同的解决方案的执行时间 意识到优化程序的重要性
--- Day01-15/Day06/code/test内置函数.py
+++ Day01-15/Day06/code/test内置函数.py
@@ -17,13 +17,26 @@ def myfilter(mystr):
 
 
 # help()
-print(chr(0x9a86))
-print(hex(ord('骆')))
+print(chr(0x8d99))
+print(hex(ord('趙')))
 print(abs(-1.2345))
-print(round(-1.2345))
-print(pow(1.2345, 5))
+print(round(1.5678,2))
+print(pow(1.2,1))
 fruits = ['orange', 'peach', 'durian', 'watermelon']
-print(fruits[slice(1, 3)])
-fruits2 = list(filter(myfilter, fruits))
-print(fruits)
-print(fruits2)
+print(fruits[slice(0,4,2)])
+print(fruits[0:4:2])
+print(list(filter(myfilter,fruits)))
+print(list(filter(None,fruits)))
+print(filter(myfilter,fruits))
+
+print(divmod(10,3))
+list1=[1,2,3,4,5]
+list2=[2,3,4,5,6]
+tuple1=(1,2,3,4)
+print(min(list2,list1))
+print(min(list2))
+print(max(list2,list1))
+print(max(list2))
+print(sum(list2))
+print(sum(tuple1,3))
+print(bin(3))
